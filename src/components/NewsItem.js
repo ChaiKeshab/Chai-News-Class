1e012b6906cf6f672beaa75479d1a81c6ec090c9
Chai Keshab <keshab3886@gmail.com>
2023-06-26 10:53:13 +0545

    switched to newsCatcherAPI (axios)

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,16 +4,16 @@ import './NewsItem.css'
 export default class NewsItem extends Component {
 
     render() {
-        let { title, link, image, description, postDate } = this.props
+        let { title, link, media, summary, postDate } = this.props
         return (
             <article className='article-newsItem'>
                 <div className='c-one'>
                     <div className='c-two'>
                         <a className='link-cta' href={link} target='_blank' rel='noreferrer'>
                             <div className='c-three'>
-                                <img className='thumbnail' src={image} alt='getBetterInternet' />
+                                <img className='thumbnail' src={media} alt='getBetterInternet' />
                                 <h1>{title}</h1>
-                                <p className='para'>{description}</p>
+                                <p className='para'>{summary}</p>
                             </div>
                         </a>
                     </div>
