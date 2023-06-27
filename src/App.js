5629f06452e2d5b5c7e8811d399a0f5c19e3321d
Chai Keshab <keshab3886@gmail.com>
2023-06-27 14:27:34 +0545

    navbar category linked with their respective type of news. Configured the api call for accepting both headlines and everyday-news with their respective parameters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import Home from './components/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 //rcc
@@ -12,12 +11,20 @@ export default class App extends Component {
     return (
       <BrowserRouter>
         <Navbar title='CHAI ' spanTitle='News' />
+
         <Routes>
-          <Route path='/Home' element={<Home />} />
-          <Route path='/' element={<News />} />
-          {/* <Route path='/Inputs' element={<Inputs Username="Your mama's name" />} />
-          <Route path='/About' element={<About />} />
-          <Route path='/Projects' element={<Projects />} /> */}
+          <Route exact path='/' element={<News getNews="latest_headlines" topic={null} lang="en" key="/ " />} />
+          <Route exact path='/chai' element={<News getNews="search" qSearch="tea OR coffee" topic={null} key="tea" />} />
+          <Route exact path='/anime' element={<News getNews="search" qSearch="anime" topic={null} key="anime" />} />
+          <Route exact path='/business' element={<News getNews="latest_headlines" topic="business" key="business" />} />
+          <Route exact path='/technology' element={<News getNews="latest_headlines" topic="tech" key="tech" />} />
+          <Route exact path='/entertainment' element={<News getNews="latest_headlines" topic="entertainment" key="entertainment" />} />
+          <Route exact path='/sport' element={<News getNews="latest_headlines" topic="sport" key="sport" />} />
+          <Route exact path='/gaming' element={<News getNews="latest_headlines" topic="gaming" key="gaming" />} />
+          <Route exact path='/music' element={<News getNews="latest_headlines" topic="music" key="music" />} />
+          <Route exact path='/beauty' element={<News getNews="latest_headlines" topic="beauty" key="beauty" />} />
+          <Route exact path='/science' element={<News getNews="latest_headlines" topic="science" key="science" />} />
+          <Route exact path='/food' element={<News getNews="latest_headlines" topic="food" key="food" />} />
         </Routes>
 
         {/* <Footer /> */}
